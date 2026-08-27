0076793ae908fa4fa572e85f9d211d2a6674e368
Jake Hauser <jake.hauser23@gmail.com>
2026-08-26 19:12:55 -0500

    Relax Cognito password requirements

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -52,11 +52,11 @@ resource "aws_cognito_user_pool" "users" {
   }
 
   password_policy {
-    minimum_length                   = 12
-    require_lowercase                = true
-    require_numbers                  = true
-    require_symbols                  = true
-    require_uppercase                = true
+    minimum_length                   = 6
+    require_lowercase                = false
+    require_numbers                  = false
+    require_symbols                  = false
+    require_uppercase                = false
     temporary_password_validity_days = 7
   }
 
